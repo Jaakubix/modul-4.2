6a07a90bb30e26403670ce56ad990730a571d991
Jakub Piskorz <kpiskorz13@gmail.com>
2021-10-30 16:48:44 +0200

    add loop that checks if the input is a palindrome

--- polindrom.py
+++ polindrom.py
@@ -1,3 +1,8 @@
 def czyPalindrom(x):
     x = x.lower().replace(" ", "")
     n = len(x)
+    for i in range(n-1):
+        if x[i] != x[n-1-i]:
+            return False
+        else:
+            return True
