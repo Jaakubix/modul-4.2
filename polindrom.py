4dc41e6cde77d28f857c5a50340f8d885a738846
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-02 11:43:39 +0100

    make changing characters more automatical

--- polindrom.py
+++ polindrom.py
@@ -1,14 +1,18 @@
-def czyPalindrom(x):
-    x = x.lower().replace(" ", "").replace("?", "").replace("!", "").replace("-", "").replace(",", "").replace(".", "")
+x = input("Podaj słowo lub zdanie: ")
+def czyPolindrom(x):
+    for letters in x:
+        letter = letters.isalpha()
+        if letter == True:
+            pass
+        else:
+            x = x.replace(letters, "").lower()
     n = len(x)
     for i in range(n-1):
         if x[i] != x[n-1-i]:
             return False
         else:
             return True
-print("Podaj słowo lub zdanie: ")
-x = input()
-if czyPalindrom(x) == True:
+if czyPolindrom(x) == True:
     polindrom = f" '{x}' jest polidromem"
 else:
     polindrom = f" '{x}' nie jest polidromem"
